80535de603bf52fbf1dc9beb38bd1f4ce1706e96
RAJESH-GN <rajeshgn821@gmail.com>
2021-05-02 08:36:43 +0200

    changes for adding image

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const MaterialComponents = [
   MatCardModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatToolbarModule
 ];
 
 @NgModule({
