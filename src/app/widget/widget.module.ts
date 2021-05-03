ea1b79217ac50e19d15fc79d3976cfb94492107b
RAJESH-GN <rajeshgn821@gmail.com>
2021-05-03 15:43:35 +0200

    splitted more files

--- src/app/widget/widget.module.ts
+++ src/app/widget/widget.module.ts
@@ -1,31 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import {
   routingComponents,
   WidgetRoutingModule,
 } from './widget-routing.module';
-import { WeatherinfoWidgetComponent } from './components/weatherinfo-widget/weatherinfo-widget.component';
-import { HttpClientModule } from '@angular/common/http';
-import { WeatherDetailsService } from './services/weather-details.service';
-import { MaterialModule } from '../material/material.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from '../shared/shared.module';
-import { ExpansionTitleComponent } from './components/expansion-title/expansion-title.component';
-import { ExpansionDescriptionComponent } from './components/expansion-description/expansion-description.component';
-import { ExpansionAdditionalInfoComponent } from './components/expansion-additional-info/expansion-additional-info.component';
-import { ExpansionAverageTemperatureComponent } from './components/expansion-average-temperature/expansion-average-temperature.component';
+import {WeatherinfoWidgetComponent} from './components/weatherinfo-widget/weatherinfo-widget.component';
+import {HttpClientModule} from '@angular/common/http';
+import {WeatherDetailsService} from './services/weather-details.service';
+import {MaterialModule} from '../material/material.module';
+import {SharedModule} from '../shared/shared.module';
+import {ExpansionTitleComponent} from './components/expansion-title/expansion-title.component';
+import {ExpansionDescriptionComponent} from './components/expansion-description/expansion-description.component';
+import {ExpansionAdditionalInfoComponent} from './components/expansion-additional-info/expansion-additional-info.component';
+import {ExpansionAverageTemperatureComponent} from './components/expansion-average-temperature/expansion-average-temperature.component';
 
 @NgModule({
-  declarations: [WeatherinfoWidgetComponent, routingComponents, ExpansionTitleComponent, ExpansionDescriptionComponent, ExpansionAdditionalInfoComponent, ExpansionAverageTemperatureComponent],
+  declarations: [
+    WeatherinfoWidgetComponent,
+    routingComponents,
+    ExpansionTitleComponent,
+    ExpansionDescriptionComponent,
+    ExpansionAdditionalInfoComponent,
+    ExpansionAverageTemperatureComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     WidgetRoutingModule,
-    FontAwesomeModule,
     SharedModule,
     MaterialModule,
   ],
   providers: [WeatherDetailsService],
 })
-export class WidgetModule {}
+export class WidgetModule {
+}
