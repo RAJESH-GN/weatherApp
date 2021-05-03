db8521357bf64b868f40bdb499840e48bc7e6737
RAJESH-GN <rajeshgn821@gmail.com>
2021-05-03 16:55:23 +0200

    added more test cases

--- src/app/widget/components/weatherinfo-details/weatherinfo-details.component.ts
+++ src/app/widget/components/weatherinfo-details/weatherinfo-details.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {  pluck } from 'rxjs/operators';
+
 import { WeatherDetailsService } from '../../services/weather-details.service';
-import { Observable, Subject, Subscription } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
 import {Coord} from '../../models/weather-api-response';
 import {Hourly} from '../../models/weather-hourly-response';
 
