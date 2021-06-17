2c749e7a18de028437329b6db0d7a2210182cd57
RAJESH-GN <rajeshgn821@gmail.com>
2021-06-17 17:17:03 +0200

    changes to jenkins Script

--- jenkins/nodejsdocker.groovy
+++ jenkins/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('rajeshgn821/weatherApp')
+            repositoryName('rajeshgn821/weatherapp')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
